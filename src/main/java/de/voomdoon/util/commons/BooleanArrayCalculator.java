c557b7903cf6afc050b7330de20589d80bf8ed43
voomdoon <nilreb24-github20141105@yahoo.de>
2023-11-13 17:17:05 +0100

    feat: add BooleanArrayCalculator

---
+++ src/main/java/de/voomdoon/util/commons/BooleanArrayCalculator.java
@@ -0,0 +1,39 @@
+package de.voomdoon.util.commons;
+
+import java.util.List;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * DOCME add JavaDoc for
+ *
+ * @author André Schulz
+ *
+ * @since 0.1.0
+ */
+@UtilityClass
+public class BooleanArrayCalculator {
+
+	/**
+	 * DOCME add JavaDoc for method or
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 * @since 0.1.0
+	 */
+	public static boolean[][] or(boolean[][] a, boolean[][] b) {
+		boolean[][] result = new boolean[a.length][];
+
+		for (int i = 0; i < a.length; i++) {
+			result[i] = new boolean[a[i].length];
+
+			for (int j = 0; j < a[i].length; j++) {
+				result[i][j] = a[i][j] | b[i][j];
+			}
+		}
+
+		return result;
+		// TODO implement or
+	}
+}
---
+++ src/test/java/de/voomdoon/util/commons/BooleanArrayCalculatorTest.java
@@ -0,0 +1,63 @@
+package de.voomdoon.util.commons;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import de.voomdoon.testing.tests.TestBase;
+
+/**
+ * DOCME add JavaDoc for
+ *
+ * @author André Schulz
+ *
+ * @since 0.1.0
+ */
+class BooleanArrayCalculatorTest {
+
+	/**
+	 * @author André Schulz
+	 *
+	 * @since 0.1.0
+	 */
+	@Nested
+	class OrTest extends TestBase {
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_empty() throws Exception {
+			logTestStart();
+
+			boolean[][] actual = BooleanArrayCalculator.or(new boolean[0][0], new boolean[0][0]);
+
+			assertThat(actual).isEqualTo(new boolean[0][0]);
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_true_left() throws Exception {
+			logTestStart();
+
+			boolean[][] actual = BooleanArrayCalculator.or(new boolean[][] { { true } }, new boolean[][] { { false } });
+
+			assertThat(actual).isEqualTo(new boolean[][] { { true } });
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_true_right() throws Exception {
+			logTestStart();
+
+			boolean[][] actual = BooleanArrayCalculator.or(new boolean[][] { { false } }, new boolean[][] { { true } });
+
+			assertThat(actual).isEqualTo(new boolean[][] { { true } });
+		}
+	}
+}
